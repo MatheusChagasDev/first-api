380581964af201782702af236a9109b27523ae4a
Matheus <matheusmiranda.dev@gmail.com>
2023-08-29 16:43:25 -0300

    feat/models menu

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose");
 
+const itemSchema = require("./item");
+
 const menuSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    picture: String,
-    song: String,
-})
+    pokédex: {
+        type: String,
+        default: null
+    },
+    pokémon: {
+        type: String,
+        default: null
+    },
+    bag: [itemSchema], // Usando um Array de objetos para representar os itens na bag
+    trainerCard: {
+        type: String,
+        default: null
+    },
+    save: {
+        type: String,
+        default: null
+    },
+    options: {
+        type: String,
+        default: null
+    },
+});
 
 const Menu = mongoose.model("Menu", menuSchema);
 module.exports = Menu;
